3217bc4c31473bd27dc03e6a9f863157e7c89065
codeerbro <my.mac.zeroone@gmail.com>
2024-03-12 01:14:25 +0530

    The Time Project

---
+++ main.js
@@ -0,0 +1,13 @@
+//  document.querySelector('#clock')
+ const clock = document.getElementById('clock');
+
+ 
+
+   
+ 
+setInterval(function(){
+    let date = new Date();
+    // console.log(date.toLocaleTimeString());
+    clock.innerHTML = date.toLocaleTimeString();
+},1000)
+
